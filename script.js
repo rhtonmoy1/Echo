e6df953c20c1111b200fbd3a26c4217eb6c96a80
Rafiul Hasan Tonmoy <rhtonmoyhd@gmail.com>
2022-12-05 18:08:36 +0600

    api call

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
+
+// document.getElementById("tableH") = getTable.;
+
+
 const btn = document.getElementById("b1");
 btn.onclick = getAlbum;
+
 
 async function getAlbum() {
   let response = await fetch(`https://jsonplaceholder.typicode.com/photos/1`);
   let data = await response.json();
-  const pra = document.getElementById("p1");
-  pra.innerText = "albumID = " + data.albumId + " ID = " + data.id;
 
-  //   console.log(data);
-  //   for (const key in data) console.log(key);
-
-  //   Object.entries(data).forEach((entry) => {
-  //     const [key, value] = entry;
-  //     console.log(` ${value}`);
-  //   });
-  const valus = [];
-  Object.entries(data).forEach((entry) => {
-    const [key, value] = entry;
-    valus.push(String(value));
-  });
-
-  addRow("tableID", valus);
+  // addRow("tableH", createTableH(data) );
+  addRow("tableID", data );
+  // console.log(data);
   return data;
 }
 
 
+// function createTableH(data){
+//   const values = [];
+//   Object.entries(data).forEach((entry) => {
+//     const [key, value] = entry;
+//     values.push(String(key));
+//   });
+//   return values
+// }
+
+
+function createValusArray(data){
+  const values = [];
+  Object.entries(data).forEach((entry) => {
+    const [key, value] = entry;
+    values.push(String(value));
+  });
+  return values
+}
 function addRow(tableID, data) {
-    // Get a reference to the table
-    let tableRef = document.getElementById(tableID);
-  
-    // Insert a row at the end of the table
-    let newRow = tableRef.insertRow(-1);
-    
-    for(let i = 0 ; i <data.length; i++){
-        // Insert a cell in the row at index 0
+  // Get a reference to the table
+  let tableRef = document.getElementById(tableID);
+
+  // Insert a row at the end of the table
+  let newRow = tableRef.insertRow(-1);
+
+  const values = createValusArray(data);
+
+  for (let i = 0; i < values.length; i++) {
+    // Insert a cell in the row at index 0
     let newCell = newRow.insertCell(i);
-  
+
     // Append a text node to the cell
-    let newText = document.createTextNode(data[i]);
+    let newText = document.createTextNode(values[i]);
     newCell.appendChild(newText);
-    }
-
-
-
-    
   }
-  
-
+}
